cad9177be71b44da07c437705bf1bba5aa2d68f8
hasibulislam999 <hasib143sl@gmail.com>
2022-12-09 14:43:41 +0600

    delete product data redux thunk init

---
+++ client/src/redux/thunk/deleteProduct.js
@@ -0,0 +1,16 @@
+import { removeProduct } from "../actions/productActions";
+
+const deleteProductData = (id) => {
+  return async (dispatch, getState) => {
+    const res = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "DELETE",
+    });
+    const data = await res.json();
+
+    if (data.acknowledged) {
+      dispatch(removeProduct(id));
+    }
+  };
+};
+
+export default deleteProductData;
