915dd77a905d9df1e8f32d5e08cb8e9fcd9d9e65
hasibulislam999 <hasib143sl@gmail.com>
2022-12-09 14:42:26 +0600

    action type redux action type modified

--- client/src/redux/actionTypes/actionTypes.js
+++ client/src/redux/actionTypes/actionTypes.js
@@ -1,3 +1,4 @@
+//* product types
 export const ADD_TO_CART = "ADD_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const PRODUCT_LOADED = "PRODUCT_LOADED";
@@ -5,3 +6,8 @@ export const ADD_PRODUCT = "ADD_PRODUCT";
 export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
 export const ADD_TO_WISHLIST = "ADD_TO_WISHLIST";
 export const REMOVE_FROM_WISHLIST = "REMOVE_FROM_WISHLIST";
+export const LOAD_PRODUCT = "LOAD_PRODUCT";
+
+//* filter types
+export const TOGGLE_BRAND = "TOGGLE_BRAND";
+export const TOGGLE_STOCK = "TOGGLE_STOCK";
