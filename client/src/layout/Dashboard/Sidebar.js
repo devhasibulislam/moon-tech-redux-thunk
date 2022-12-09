2dc92d41038ff45d85515346da7cd43e4ad5382d
hasibulislam999 <hasib143sl@gmail.com>
2022-12-09 14:41:03 +0600

    side bar modified

--- client/src/layout/Dashboard/Sidebar.js
+++ client/src/layout/Dashboard/Sidebar.js
@@ -5,15 +5,15 @@ const Sidebar = () => {
   return (
     <div className='col-span-2 bg-indigo-200 h-[calc(100vh-25px)] p-5 rounded-lg'>
       <ul className='flex gap-3  flex-col h-full'>
-        <li>Admin Dashboard</li>
+        <li className="font-medium text-lg overflow-hidden text-ellipsis whitespace-nowrap">Admin Dashboard</li>
         <li>
-          <Link to='/dashboard'>Product List</Link>
+          <Link to='/dashboard' className="overflow-hidden text-ellipsis whitespace-nowrap">Product List</Link>
         </li>
         <li>
-          <Link to='add-product'> Add Product </Link>
+          <Link to='add-product' className="overflow-hidden text-ellipsis whitespace-nowrap"> Add Product </Link>
         </li>
         <li className='mt-auto'>
-          <Link to='/'> Back to Home </Link>
+          <Link to='/' className="overflow-hidden text-ellipsis whitespace-nowrap"> Back to Home </Link>
         </li>
       </ul>
     </div>
